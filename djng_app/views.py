1ec12a916bf90cd33ccec9a97a21055a42af0260
janchk <janchk@gmail.com>
2016-12-22 02:01:02 +0300

    Cleaned up and edit uploaded page

--- djng_app/views.py
+++ djng_app/views.py
@@ -10,23 +10,17 @@ def index(request):
     return HttpResponse("Hi, it's django_app index")
 
 
-# def show_img(request, arg):
-#     render_to_response('uploaded.html', arg)
-
-
 def upload_file(request):
     print(request)
     cntimg, simg = 'none', 'none'
     if request.method == 'POST':
-        # print('RequestMethodIsPOST')
         form = UploadFileForm(request.POST, request.FILES)
         print('FORM IS VALID =', form.is_valid())
-        # print(request.POST.pop('imgtype')[0])
-        # print('data =', request. )
         # if 'content_img' in form.files:
-        cntimg = file_handler.handle_uploaded_file(request.FILES['content_img'], imgtype='content_image')
+        cntimg = file_handler.handle_uploaded_file(request.FILES['content_img'],
+                                                   imgtype='content_image')  # send cnt_img
         # elif 'style_img' in form.files:
-        simg = file_handler.handle_uploaded_file(request.FILES['style_img'], imgtype='style_img')
+        simg = file_handler.handle_uploaded_file(request.FILES['style_img'], imgtype='style_img')  # send style_img
         # else:
         #     HttpResponse('Ой. Какая неожиданнсть.')
     else:
@@ -35,4 +29,4 @@ def upload_file(request):
     return render(request, 'uploaded.html', {'form': form, 'cimg': cntimg, 'simg': simg})
     # страница после загрузки файла
 
-# Create your views here.
+    # Create your views here.
