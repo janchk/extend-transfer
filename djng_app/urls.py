8a9b05d5a94048416c4f80c6fa0a8272998ba357
jan <janchk@gmail.com>
2017-02-15 19:14:55 +0300

    All is great. Still problems with caffe I guess.

--- djng_app/urls.py
+++ djng_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='file_upload.html'), name='files'),
     url(r'^proceed', processing, name='proceed'),
     url(r'^upload', upload_file, name='upload_file'),
+    url(r'^pbar', pbar_mng, name='pbar'),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
